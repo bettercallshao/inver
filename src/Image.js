73ce5ffa7c07a02735b53d6d5f48e21b919c157a
stableorbit <landon.g.chang@gmail.com>
2021-11-06 00:17:33 -0700

    cache images and refetch after 10mins

--- src/Image.js
+++ src/Image.js
@@ -1,26 +1,35 @@
 import { Storage } from "@aws-amplify/storage";
 import { useState, useEffect } from "react";
+import { IMAGE_URL_EXPIRATION } from "./constants";
+import { addToCache, getFromCache } from "./helpers/cache";
 
 function Image({ imgKey, actions }) {
   const [dig, setDig] = useState(false);
   const [src, setSrc] = useState(null);
   useEffect(() => {
     (async () => {
-      const url = await Storage.get(imgKey, { level: "private" });
-      setSrc(url);
+      const cachedUrl = getFromCache(imgKey);
+      if (cachedUrl) {
+        setSrc(cachedUrl);
+      } else {
+        const url = await Storage.get(imgKey, { level: "private", expires: IMAGE_URL_EXPIRATION });
+        setSrc(url);
+        addToCache(imgKey, url);
+      }
     })();
-  }, []);
+  });
   return (
-    <div class="p-0">
+    <div className="p-0">
       <img
         className="img-fluid border"
         src={src}
         onClick={() => {
           setDig(!dig);
         }}
+        alt={imgKey}
       />
       {dig && actions && (
-        <div class="d-flex justify-content-between">
+        <div className="d-flex justify-content-between">
           {actions.map((action) => (
             <button
               key={action.text}
